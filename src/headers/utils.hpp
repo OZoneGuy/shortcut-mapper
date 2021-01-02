44506eb6c2fd131d2f4b34a212a61f71e20c3b56
OZoneGuy <alkersho@protonmail.com>
2021-01-02 22:40:29 +0300

    feat - Added helper functions to get keycodes
    Added a function to get keycode from string and a function to get char from keycode

--- src/headers/utils.hpp
+++ src/headers/utils.hpp
@@ -1,5 +1,11 @@
 #pragma once
 
+#include <X11/X.h>
+#include <X11/Xlib.h>
 #include <string>
 
 void write_log(int priority, const std::string &msg);
+
+KeyCode keycode_from_string(Display *dpy, const std::string& key);
+
+char char_from_keycode(const XEvent &ev);
