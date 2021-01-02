8987e20c57c5615c9200b96654e9539ac1ba1eca
OZoneGuy <alkersho@protonmail.com>
2021-01-02 22:42:16 +0300

    feat - Implemented Keycode functions from 44506eb

--- src/utils.cpp
+++ src/utils.cpp
@@ -7,3 +7,11 @@ void write_log(int priority, const std::string &msg){
   std::time_t time = std::time(nullptr);
   syslog(priority, "%s: %s", std::ctime(&time), msg.c_str());
 }
+
+KeyCode keycode_from_string(Display *dpy, const std::string &key){
+  return XKeysymToKeycode(dpy, XStringToKeysym(key.c_str()));
+}
+
+char char_from_keycode(const XEvent &ev){
+  return XKeysymToString(XLookupKeysym((XKeyEvent *)&ev, 0))[0];
+}
