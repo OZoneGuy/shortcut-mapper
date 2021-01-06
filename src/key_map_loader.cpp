25897a8b174a04087d624edcf123517ebaecea2f
OZoneGuy <alkersho@protonmail.com>
2021-01-06 16:54:58 -0500

    chore - Formatted files

--- src/key_map_loader.cpp
+++ src/key_map_loader.cpp
@@ -2,13 +2,14 @@
 #include "headers/key_map_node.hpp"
 #include "headers/utils.hpp"
 #include <X11/X.h>
+#include <X11/Xlib.h>
+#include <cstdlib>
+#include <fstream>
+#include <sstream>
 #include <stdexcept>
+#include <string>
 #include <sys/syslog.h>
 #include <syslog.h>
-#include <sstream>
-#include <string>
-#include <fstream>
-#include <X11/Xlib.h>
 
 using std::invalid_argument;
 using std::istringstream;
@@ -17,86 +18,51 @@ using std::string;
 KeyMapNode *load_key_map(string &file_name) {
   std::ifstream file(file_name);
 
+  // Try opening file
   if (!file.is_open()) {
-    write_log(LOG_ERR,
-              string("Could not open file at: ") + file_name);
+    // Log failure to open file
+    write_log(LOG_ERR, string("Could not open file at: ") + file_name);
     return nullptr;
   }
+
   string line;
-  KeyMapNode *key_map = new KeyMapNode('\0');
+  KeyMapNode *key_map = new KeyMapNode(
+      '\0'); // Root node, has no commands and cannot be activated
+
   while (getline(file, line)) {
-    if (line == "") // empty line
+    if (line == "") // empty line, skip
       continue;
-    string keys;
+
+    string keys; // holds the first non space terminated string, usually holds
+                 // the key string
     istringstream iss(line);
     iss >> keys;
-    if (keys[0] == '#') // comment line
+    if (keys[0] == '#') // commented line
       continue;
 
     // Register master hotkey
     if (keys == "Master:") {
-      iss >> keys;
+      iss >> keys; // Store the master key bind
 
-      Display *dpy;
-      int i = 0;
-      unsigned int mods = 0;
-      char code = '\0';
-      bool failed = false;
-      while (i < keys.size()){
-        if (i == keys.size() - 1) {
-          code = keys[i];
-          break;
-        }
-        if (keys[i] == 'C') {
-          if (i + 2 < keys.size() && keys[i + 1] == '-')
-            mods |= ControlMask;
-          else {
-            failed = true;
-            break;
-          }
-        }
-        if (keys[i] == 'A') {
-          if (i + 2 < keys.size() && keys[i + 1] == '-')
-            mods |= Mod1Mask;
-          else {
-            failed = true;
-            break;
-          }
-        }
-        if (keys[i] == 'S') {
-          if (i + 2 < keys.size() && keys[i + 1] == '-')
-            mods |= Mod4Mask;
-          else {
-            failed = true;
-            break;
-          }
-        }
-      }
-      if (failed) {
-        write_log(LOG_WARNING,
-                  string("Bad Master Key at character: ") + std::to_string(i) +
-                  string("\tDefaulting to C-S-k."));
-        code = 'k';
-        mods = ControlMask | Mod4Mask;
-      }
-      if (NULL == (dpy = XOpenDisplay(0))) {
-        write_log(LOG_ERR, "Failed to acquire display.");
-        exit(1);
-      }
-      XGrabKey(dpy, keycode_from_string(dpy, std::to_string(code)), mods,
-               DefaultRootWindow(dpy), 0, GrabModeAsync, GrabModeAsync);
+      grab_master(keys); // grab the key bind
       continue;
     }
 
     // register key string
     string cmd;
-    iss >> cmd;
+    getline(iss, cmd);   // get the remainder of the line
+    cmd = cmd.substr(1); // remove first character, it is a space
     try {
-      key_map->add(keys, cmd);
-    } catch (const invalid_argument& error) {
+      key_map->add(keys, cmd); // try adding the command to the key tree
+    } catch (const invalid_argument &error) {
       write_log(LOG_ERR, error.what());
     }
   }
+
+  // close file
+  file.close();
+
+  // Return key map tree
   return key_map;
 }
 
@@ -115,7 +81,7 @@ void grab_master(std::string keys) {
   }
 
   // Read and parse key bind
-  while (i < keys.size()){
+  while (i < keys.size()) {
 
     // there is only one character (remaining)
     if (i == keys.size() - 1) {
@@ -159,9 +125,9 @@ void grab_master(std::string keys) {
 
   // Failed to parse the key, defaulting master key to C-S-k (Control+Super+k)
   if (failed) {
-    write_log(LOG_WARNING,
-              string("Bad Master Key at character: ") + std::to_string(i) +
-              string("\tDefaulting to C-S-k."));
+    write_log(LOG_WARNING, string("Bad Master Key at character: ") +
+                               std::to_string(i) +
+                               string("\tDefaulting to C-S-k."));
     code = 'k';
     mods = ControlMask | Mod4Mask;
   }
--- src/utils.cpp
+++ src/utils.cpp
@@ -1,18 +1,20 @@
 #include "headers/utils.hpp"
 #include <ctime>
-#include <sys/syslog.h>
 #include <string>
+#include <sys/syslog.h>
 
-void write_log(int priority, const std::string &msg){
+using std::string;
+
+void write_log(int priority, const std::string &msg) {
   std::time_t time = std::time(nullptr);
   syslog(priority, "%s: %s", std::ctime(&time), msg.c_str());
 }
 
-KeyCode keycode_from_string(Display *dpy, const std::string &key){
+KeyCode keycode_from_string(Display *dpy, const std::string &key) {
   return XKeysymToKeycode(dpy, XStringToKeysym(key.c_str()));
 }
 
-char char_from_keycode(const XEvent &ev){
+char char_from_keycode(const XEvent &ev) {
   return XKeysymToString(XLookupKeysym((XKeyEvent *)&ev, 0))[0];
 }
 
